7e92fc40685a3ecdd2ac551971b7eb76049fab28
kalleby santos <kalleby_santos@hotmail.com>
2023-05-24 16:50:49 +0100

    📦 NEW: Creating Example task

---
+++ TaskRunner.Example/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using TaskRunner.Lib;
+
+namespace TaskRunner.Example;
+
+public record HelloWorldParameters(string name);
+
+public class HelloWorld : ITaskExecutor
+{
+    private readonly ILogger<HelloWorld> _logger;
+
+    public HelloWorld(ILogger<HelloWorld> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task<object?> ExecuteAsync(object? parameters, CancellationToken cancellationToken)
+    {
+        var values = parameters as HelloWorldParameters;
+
+        _logger.LogInformation($"Hello World {values?.name}");
+
+        return Task.FromResult<object?>(null);
+    }
+}
